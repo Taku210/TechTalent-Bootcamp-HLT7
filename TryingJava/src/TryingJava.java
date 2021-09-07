a0b7b722c8e0d096148abf1435c3e110dcb11ca9
Taku210 <87713046+Taku210@users.noreply.github.com>
2021-09-07 23:25:56 +0100

    Add files via upload

---
+++ TryingJava/src/TryingJava.java
@@ -0,0 +1,17 @@
+public class TryingJava {
+    public static void main(String[] args) {
+        int a=5;
+        float b=4.5f;
+        double c=4.5;
+        boolean result=true;
+        char d='A';
+        String name="Peter";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(true);
+        System.out.println(d);
+        System.out.println(name);
+
+    }
+}
